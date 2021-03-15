286978fab0b75a0fcf7b1567c0060cf7c5fa262f
Fabian Diemand <fabian.diemand@swisscom.com>
2021-03-15 23:39:17 +0100

    feature(pva4/producerconsumer): solve exercise

--- src/main/java/pva04/producerconsumer/Counter.java
+++ src/main/java/pva04/producerconsumer/Counter.java
@@ -2,7 +2,7 @@ package pva04.producerconsumer;
 
 public class Counter extends Thread {
 
-	private Storage storage;
+	private final Storage storage;
 	private int max, min;
 
 	/**
@@ -28,7 +28,30 @@ public class Counter extends Thread {
 	 */
 	@Override
 	public void run() {
-		// TODO Implementieren Sie den Zaehler
+
+		// use storage object as sync lock
+		synchronized(storage) {
+			while (!Thread.currentThread().isInterrupted()) {
+				try {
+					while (min <= max) {
+
+						// post increment: set value, THEN increment
+						storage.setValue(min++);
+
+						//notify monitor about finished action and wait
+						storage.notify();
+						storage.wait();
+					}
+
+					// interrupt thread if max is reached to stop the counter
+					Thread.currentThread().interrupt();
+				} catch (InterruptedException iex) {
+
+					// reset interrupt, bc the catch block swallows it otherwise
+					Thread.currentThread().interrupt();
+				}
+			}
+		}
 	}
 
 }
