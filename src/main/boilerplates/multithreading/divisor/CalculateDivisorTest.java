4832f04851a70f43c14af8ffa37f3f257a51657f
Fabian Diemand <fabian.diemand@swisscom.com>
2021-03-16 13:43:56 +0100

    fix: replace System-Rules for all tests

--- src/main/boilerplates/multithreading/divisor/CalculateDivisorTest.java
+++ src/main/boilerplates/multithreading/divisor/CalculateDivisorTest.java
@@ -1,12 +1,12 @@
 package ch.ffhs.ftoop.multithreading.divisor;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.jupiter.api.Test;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.*;
 
 import java.util.concurrent.ExecutionException;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author ble
@@ -15,16 +15,13 @@ import static org.junit.Assert.assertEquals;
 
 public class CalculateDivisorTest {
 
-	@Rule
-	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
-
-
 	@Test
-	public void testCalculate() throws InterruptedException, ExecutionException {
-		CalculateDivisor.main(new String[] { "10", "10000", "4" });
+	public void testCalculate() throws InterruptedException, ExecutionException, Exception {
+		String text =  tapSystemOutNormalized(() -> CalculateDivisor.main(new String[] { "10", "10000", "4" }));
+
 		assertEquals(
 				"Ergebnis: Zahl mit maximaler Anzahl Divisoren: 7560 (64 Divisoren)\n",
-				systemOutRule.getLog());
+				text);
 	}
 
 }
